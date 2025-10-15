a3fda980b8b89752378fd3f3754355c63d0e3bc2
Dionisii Grinko <DionisiyGr@gmail.com>
2025-10-15 09:30:05 -0400

    add csv parser implementation

--- parser/csv.go
+++ parser/csv.go
@@ -1,1 +1,25 @@
 package parser
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+)
+
+type Parser interface {
+	Parse(file io.Reader) ([][]string, error)
+}
+
+type fileParser struct{}
+
+func New() Parser {
+	return &fileParser{}
+}
+
+func (fp *fileParser) Parse(file io.Reader) ([][]string, error) {
+	c, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("invalid file content: %w", err)
+	}
+	return c, nil
+}
